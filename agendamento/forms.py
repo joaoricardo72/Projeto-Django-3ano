503742960e0d37ed4e852cc0bced9919f6909c09
joaoricardo72 <108432022@eniac.edu.br>
2024-10-30 18:25:12 +0000

    Pending changes exported from your codespace

--- agendamento/forms.py
+++ agendamento/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 from .models import Agendamento
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 
 class AgendamentoForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Agendamento
+        fields = ['data', 'espaco', 'descricao']
+        widgets = {
+            'data' : forms.DateInput(attrs={'type' : 'date'})
+        }
+    def __init__ (self, *args, **kwargs):
+        super(AgendamentoForm, self).__init__(*args, **kwargs)
+        self.helper=FormHelper()
+        self.helper.add_input(Submit('submit', 'Agendar'))
